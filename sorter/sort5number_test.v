ec14bbce05b932bbc6cce132a0ea10fa42392876
Vasin Wongrassamee <vw214@corona16.doc.ic.ac.uk>
2016-07-08 17:59:09 +0100

    sort5numbers done and tested

---
+++ sorter/sort5number_test.v
@@ -0,0 +1,31 @@
+module sort5number_test
+  #(parameter 
+      data_width = 3)
+  (
+      input wire clk,
+      output wire [4 * data_width - 1 : 0] outp_inps,
+      output wire [(data_width*4) -1 :0] outp
+  );
+
+  localparam
+      num_bits = data_width * 4;
+
+      //counter 
+      reg [4 * data_width - 1 : 0] count;
+      initial begin
+          count = 686;
+      end
+      always @ (posedge clk)
+          count <= count + 1;
+      assign outp_inps = count;
+
+      // instantiate 
+      sort5number
+          #(
+                .DW(data_width)
+          ) my_inner_product (
+                .inp(count),
+                .outp(outp)
+          );
+
+endmodule
