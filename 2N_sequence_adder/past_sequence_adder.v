01978ca7838924854c22c20fb4917e8e94830ced
Vasin W <vasinwr@gmail.com>
2016-07-14 17:17:21 +0100

    added debugging code, past sequence adder still has bug

--- 2N_sequence_adder/past_sequence_adder.v
+++ 2N_sequence_adder/past_sequence_adder.v
@@ -17,7 +17,8 @@ module past_sequence_adder
     assign sums[1] = regs[1] + inp;
     generate 
     for (i = 2; i <= N; i = i + 1) begin: sum_loop
-        assign sums[i] = sums[i-1] + regs[2**(N+i-2) + 2**(i-1)];
+       // assign sums[i] = sums[i-1] + regs[2**(N+i-2) + 2**(i-1)];
+        assign sums[i] = sums[i-1] + regs[2**(N+i-2) + i];
     end
     endgenerate  
 
@@ -31,11 +32,15 @@ module past_sequence_adder
 
         regs[1] <= inp;
         for (j = 2; j <= N; j = j + 1) begin
+          $display("A: j=",j," k=",k);
           for (k = 1; k <= j; k = k + 1) begin
+            $display("B: j=",j," k=",k);
             if(k == 1) begin
+                  $display("B1");
                   regs[2**(N+j-2)+k] <= sums[j-1];
             end 
             else begin
+                  $display("B2");
                   regs[2**(N+j-2)+k] <= regs[2**(N+j-2)+k-1];
             end 
           end           
