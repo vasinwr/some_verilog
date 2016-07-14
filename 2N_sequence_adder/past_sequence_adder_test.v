346a4a3eec20632edfd676602e8797e454cf6e40
Vasin W <vasinwr@gmail.com>
2016-07-14 11:19:50 +0100

    made tests, but has bug

---
+++ 2N_sequence_adder/past_sequence_adder_test.v
@@ -0,0 +1,32 @@
+module past_sequence_adder_test
+    #(parameter
+        data_width = 8,
+        N = 4
+    )
+    (
+        input wire clk,
+        output wire [data_width - 1: 0] outp,
+        output wire [data_width - 1: 0] outp_inps
+    );
+
+    reg [data_width-1: 0] count;
+
+    initial begin
+      count = 0;
+    end
+
+    always @ (posedge clk)
+        count <= count + 1;
+    assign outp_inps = count;
+
+    past_sequence_adder
+        #(
+            .N(N),
+            .DW(data_width)
+        ) my_past_sequence_adder (
+            .inp(count),
+            .outp(outp)
+        );
+
+endmodule
+
