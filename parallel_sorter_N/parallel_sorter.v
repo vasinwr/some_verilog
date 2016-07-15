d53d0eee307e2c159def07925527703463dfaca2
Vasin W <vasinwr@gmail.com>
2016-07-15 18:51:22 +0100

    zzzzz as output, bug not yet found

--- parallel_sorter_N/parallel_sorter.v
+++ parallel_sorter_N/parallel_sorter.v
@@ -8,37 +8,37 @@ module parallel_sorter
         output wire [(DW*N)-1 : 0] outp
     );
 
-    localparam row = N;
-    localparam column = N*2;
+    localparam col = N;
+    localparam row = N*2;
 
-    wire [DW-1 : 0] wire_grid [0: row*column -1]; // wire_grid [0:row][0:column]
+    wire [DW-1 : 0] wire_grid [0: row*col -1]; // wire_grid [0:row][0:column] ,[y-axis][x-axis]
 
+
+    //why cant this be a for loop in 'initial'?
     genvar r;
     generate
       // takes care edge cases
-        for (r = 0; r < row; r = r + 1) begin       
-            //assign wire_grid[r][1+(r*2)] = wire_grid[r][r*2];  
-            assign wire_grid[(r*row) + (r*2) + 1] = wire_grid[(r*row) + (r*2)];
+        for (r = 1; r < row; r = r + 2) begin       
+            //assign wire_grid[r][(r-1)/2] = wire_grid[r-1][(r-1)/2];  
+            assign wire_grid[(r*col) + (r-1)/2] = wire_grid[(r-1)*col + (r-1)/2];
         end
       // assign outputs
         for (r = 0; r < N; r = r + 1) begin
-            //assign outp[DW*(r+1)-1 : DW*r] = wire_grid[r*2+1][row-1]; 
-            assign outp[DW*(r+1)-1 : DW*r] = wire_grid[(r*2+1)*row + row-1];
+            //assign outp[DW*(r+1)-1 : DW*r] = wire_grid[r*2+1][col-1]; 
+            assign outp[DW*(r+1)-1 : DW*r] = wire_grid[(r*2+1)*col + col-1];
         end 
     endgenerate
 
     
     genvar i,j;
     generate
-        for (i = 1; i < row; i = i+1) begin 
-            for (j = 1; j < column-(row-1-i)*2 ; j = j+2) begin  
-                if (j != column -1) begin // edge case: last column doesn't have a comparator 
-                  comparator #(.DW(DW)) cmp ( .inp1(wire_grid[(i-1)*row + j]),    //[i-1][j]
-                                              .inp2(wire_grid[i*row + (j-1)]),    //[i][j-1]
-                                              .out_min(wire_grid[(i+1)*row + j]), //[i+1][j]
-                                              .out_max(wire_grid[i*row + (j+1)])  //[i][j+1]
+        for (i = 1; i < col; i = i+1) begin 
+            for (j = 1; j < i*2 ; j = j+2) begin  
+                  comparator #(.DW(DW)) cmp ( .inp1(wire_grid[i*col + (j-1)]),    //[i][j-1]
+                                              .inp2(wire_grid[(i-1)*col + j]),    //[i-1][j]
+                                              .out_min(wire_grid[i*col + j]),     //[i][j]
+                                              .out_max(wire_grid[(i+1)*col + j])  //[i+1][j]
                                             );
-                end
             end
         end 
     endgenerate
