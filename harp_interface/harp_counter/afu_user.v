705d1c4e1155fc198827fef725bff61a193ff140
Vasin W <vasinwr@gmail.com>
2016-09-09 14:52:39 +0100

    simplify interface to minimal function and counter

--- harp_interface/harp_counter/afu_user.v
+++ harp_interface/harp_counter/afu_user.v
@@ -90,28 +90,15 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512)
 
    reg [2:0] 		   r_state, n_state;
    reg 			   r_done,n_done;
-   reg 			   t_start_pe;
+
+   /* added: counter stuff */
+   reg [63:0] r_counter;
    
    assign done = r_done;
-
-   /* pe done */
-   wire 		   w_pe_done;
-   /* found cycle */
-   wire 		   w_cycle;
-   /* pe read address */
-   wire [63:0] 		   w_pe_addr;
-   /* pe address valid */
-   wire 		   w_pe_addr_val;
 
    /* combinational signal to pop address from pe */
    reg 			   t_pop_addr;
 
-   /* take offset from base */
-   wire [63:0] w_pe_baddr = w_pe_addr + (~r_base_ptr + 64'd1);
-   /* convert to cacheline */
-   wire [ADDR_LMT-1:0] w_pe_claddr = w_pe_baddr[(ADDR_LMT+5):6];
-   /* save byte offset for metadata */
-   wire [5:0] 	       w_pe_offset = w_pe_baddr[5:0];
    /* padding for metadata */
    wire [MDATA-6-1:0]  w_meta_pad = 'd0;
 
@@ -127,21 +114,7 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512)
 		       (rd_rsp_mdata == 'd48) ? rd_rsp_data[447:384] :
 		       rd_rsp_data[511:448];
    
-   cycle_detection_pe pe (
-			  // Outputs
-			  .done			(w_pe_done),
-			  .cycle		(w_cycle),
-			  .addr			(w_pe_addr[63:0]),
-			  .addr_val		(w_pe_addr_val),
-			  // Inputs
-			  .clk			(clk),
-			  .rst			(rst),
-			  .start		(t_start_pe),
-			  .head_addr		(r_head_ptr),
-			  .pop_addr		(t_pop_addr),
-			  .din			(w_reply_data),
-			  .din_val		(rd_rsp_valid)
-			  );
+
       
    always@(posedge clk)
      begin
@@ -156,6 +129,9 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512)
 	r_wr_req_data <= rst ? 'd0 : n_wr_req_data;
 	r_base_ptr <= rst ? 'd0 : n_base_ptr;
 	r_head_ptr <= rst ? 'd0 : n_head_ptr;
+        if(rst) begin
+            r_counter <= 'd0;
+        end
      end
 
    /* read request FSM */
@@ -175,7 +151,6 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512)
 	
 	n_base_ptr = r_base_ptr;
 	n_head_ptr = r_head_ptr;
-	t_start_pe = 1'b0;
 	t_pop_addr = 1'b0;
 	
 	case(r_state)
@@ -191,49 +166,31 @@ module afu_user #(ADDR_LMT = 20, MDATA = 14, CACHE_WIDTH = 512)
 	    end
 	  'd1:
 	    begin
-	       t_start_pe = 1'b1;
-	       n_state = 'd2;
+               $display("counter value: %d", r_counter);
+               r_counter = r_counter + 1;
+               if(r_counter == 'd100) begin
+	         n_state = 'd2;
+               end
 	    end
 	  'd2:
 	    begin
-	       if((!rd_req_almostfull) && w_pe_addr_val)
+	       /* write back the value of counter*/
+	       if(!wr_req_almostfull)
 		 begin
-		    //$display("sending request for %x", w_pe_baddr);
-		    //$display("cci addr %x", w_pe_claddr);		    
-		    n_rd_req_addr = w_pe_claddr;
-		    /* save byte offset in metadata so we
-		     * know the desired offset in the cache line
-		     * reply */
-		    n_rd_req_mdata = {w_meta_pad, w_pe_offset};
-		    n_rd_req_en = 1'b1;
-		    t_pop_addr = 1'b1;
-		    n_state = 'd2;
-		 end
-	       else if(w_pe_done)
-		 begin
+		    n_wr_req_addr = 'd0;
+		    n_wr_req_data = r_counter;
+		    n_wr_req_en = 1'b1;
 		    n_state = 'd3;
 		 end
 	    end
 	  'd3:
 	    begin
-	       /* write back to indicate if a cycle
-		* was found in the linked list */
-	       if(!wr_req_almostfull)
+	       if(wr_rsp0_valid || wr_rsp1_valid)
 		 begin
-		    n_wr_req_addr = 'd0;
-		    n_wr_req_data = {512{w_cycle}};
-		    n_wr_req_en = 1'b1;
 		    n_state = 'd4;
 		 end
 	    end
 	  'd4:
-	    begin
-	       if(wr_rsp0_valid || wr_rsp1_valid)
-		 begin
-		    n_state = 'd5;
-		 end
-	    end
-	  'd5:
 	    begin
 	       n_done = 1'b1;
 	    end
